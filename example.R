92656c84765299b897c91b559a127ca94705dfe0
Gorzo <galaxycoff@gmail.com>
2025-10-10 16:13:15 -0400

    fixing example script

--- example.R
+++ example.R
@@ -8,7 +8,7 @@ library(mriptools)
 # the areas, and the modes that you would like included on your graphs
 # You might want to watch how many species are included as it may take a little bit for the graphing function to loop through a ton of species
 
-myspecies <- c(
+species <- c(
   "ATLANTIC CROAKER",
   "BLACK DRUM",
   "BLACK SEA BASS",
@@ -30,5 +30,5 @@ mrip(
   areas = c("INLAND", "OCEAN (<= 3 MI)", "OCEAN (> 3 MI)"),
   modes = c("CHARTER BOAT", "PARTY BOAT", "PRIVATE/RENTAL BOAT", "SHORE"),
   state = 24,
-  out_dir) # in_dir="~/data/mrip_old",
+  out_dir = out_dir) # in_dir="~/data/mrip_old",
 
