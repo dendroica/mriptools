f2b081948e168fb5244d8d973f6815d96a601aca
Gorzo <galaxycoff@gmail.com>
2025-10-22 13:53:46 -0400

    simplify function output

--- R/mrip.R
+++ R/mrip.R
@@ -111,7 +111,8 @@ MRIPOutlier <- function(catch, effort, start_yr, end_yr, prelim_yr, species) {
 
   compute_subset <- base_effort[, c("WAVE", "MODE_FX_F", "AREA_X_F", "ESTRIPS")]
   trips <- outlie("ESTRIPS", compute_subset, effort_prelim, c("WAVE", "MODE_FX_F", "AREA_X_F"), out_dir)
-  return(list(outlierx, trips))
+  outlierx <- c(outlierx, trips)
+  return(outlierx)
 }
 
 #' MRIP analysis
@@ -155,8 +156,7 @@ mrip <- function(start_yr, end_yr, prelim_yr, species, waves, areas, modes, stat
   combined_catch <- MRIPOutlier(
     mrip_data[[1]], mrip_data[[2]], start_yr, end_yr, prelim_yr, species
   )
-  writeout(combined_catch[[1]], out_dir)
-  writeout(combined_catch[[2]], out_dir)
+  writeout(combined_catch, out_dir)
   makeplots(mrip_data[[1]], mrip_data[[2]], species, waves, out_dir)
 }
 
