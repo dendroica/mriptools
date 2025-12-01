bfc23e32e2d276003ddf71cc7eb23ee935c1c4aa
Gorzo <galaxycoff@gmail.com>
2025-12-01 16:06:05 -0500

    cleaning up example script

--- example.R
+++ example.R
@@ -8,10 +8,8 @@ library(mriptools)
 # the areas, and the modes that you would like included on your graphs
 # You might want to watch how many species are included as it may take a little bit for the graphing function to loop through a ton of species
 
-species <- c(
-  "BLACK SEA BASS"
-)
-out_dir <- "~/output/mrip/mrip_ex2"
+species <- c("TAUTOG","BLACK SEA BASS")
+out_dir <- "~/output/mrip/mrip_ex"
 mrip(
   comparison_timespan = 2017:2024,
   prelim_yr = 2025,
